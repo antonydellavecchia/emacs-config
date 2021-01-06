344136a50b3a41af81b6e8397d57ebaabbc7bbe2
Antony <antony@live-eo.com>
2021-01-06 10:39:14 +0100

    liveeo adjustments

--- init.el
+++ init.el
@@ -28,7 +28,6 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package counsel)
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper))
@@ -190,21 +189,24 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-safe-themes
-   (quote
-    ("3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" "a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default)))
- '(global-command-log-mode t)
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(counsel-describe-function-function 'helpful-callable t)
+ '(counsel-describe-variable-function 'helpful-variable t)
+ '(custom-enabled-themes '(wheatgrass))
+ '(doom-modeline-height 15 t)
+ '(gnus-select-method '(nnreddit ""))
+ '(helm-completion-style 'emacs)
+ '(ivy-wrap t t)
+ '(js-indent-level 2)
  '(package-selected-packages
-   (quote
-    (elpy dired dired-single conda gradle-mode kotlin-mode js2-mode lsp-mode counsel-projectile counsel-projectile projectile general doom-modeline ivy-rich doom-themes helpful which-key rainbow-delimiters multiple-cursors counsel ivy yaml-mode use-package smartparens sage-shell-mode plsense)))
- '(safe-local-variable-values (quote ((projectile-project-run-cmd . "npm start")))))
+   '(all-the-icons helm-tramp docker-tramp elpygen pygen emacsql-psql emacsql conda smartparens pyenv-mode-auto pyenv-mode elpy flymake-jslint helm sphinx-doc dockerfile-mode magit treemacs circe el-get use-package flymake-json ssh-config-mode yaml-mode multiple-cursors rjsx-mode js2-mode restclient solaire-mode rubik restclient-test))
+ '(projectile-completion-system 'ivy t)
+ '(pyvenv-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#FFFFFF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "CTDB" :family "Fira Code"))))
- '(cursor ((t (:background "green")))))
+ )
 
 
